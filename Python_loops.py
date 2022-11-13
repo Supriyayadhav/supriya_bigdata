a769c5d703e311c6085dd0b7351cd277b042bc50
Supriyayadhav <117853745+Supriyayadhav@users.noreply.github.com>
2022-11-13 13:31:26 +0530

    Create Python_loops.py

---
+++ Python_loops.py
@@ -0,0 +1,13 @@
+#practicing loops in python
+#for loop to define 9 table
+#num_var=9
+#result_var=1
+#for i in range(1,11) :
+ #   result_var=num_var*i
+  #  print(num_var ,' * ' ,i,' = ', result_var)
+#Printing 9 table in reverse
+num_var=9
+result_var=1
+for i in range(10,0,-1) :
+    result_var=num_var*i
+    print(num_var ,' * ' ,i,' = ', result_var)
